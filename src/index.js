4c6ded4ce7e888bccd12a35d5781e27f9271fd0c
Ludwig Richter <richter@fliegwerk.com>
2019-09-10 10:20:06 +0200

    Added Exception Handling in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const fsp = require('fs').promises;
 const parser = require('./parser.js');
 
+const mdstatPath = '/proc/mdstat';
+
 /**
  * The Linux RAID module for the server-state system
  * @returns A JSON-serializable (via `JSON.stringify()`) version information about the raids in the linux system
  */
 module.exports = async function() {
-    const file = await fsp.readFile('./examples/mdstat/typical.mdstat', {encoding: 'utf-8'});
-    return parser.parse(file);
+    try {
+        var file = await fsp.readFile(mdstatPath, {encoding: 'utf-8'});
+        return parser.parse(file);
+    } catch (e) {
+        // mdraid kernel module not loaded. file missing.
+        return {'error': 'kernel module not loaded'};
+    }
 };
