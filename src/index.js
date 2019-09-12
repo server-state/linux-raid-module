922bda25f57f57d8ea01ad985605f4695b23225f
Ludwig Richter <richter@fliegwerk.com>
2019-09-13 01:21:45 +0200

    Added unique naming - unique naming - extended fs mock for further unit testing - added special unit tests

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 const fs = require('fs');
+const path = require('path');
 const parser = require('./grammar.pegjs');
 
 /**
  * The Linux RAID module for the server-state system
  * @returns A JSON-serializable (via `JSON.stringify()`) version information about the raids in the linux system
  */
-module.exports = async function() {
+module.exports = async function () {
     let file;
     // pull file from filesystem
     try {
-        file = fs.readFileSync('/proc/mdstat', {encoding: 'utf-8'});
+        file = fs.readFileSync('/proc/mdstat', { encoding: 'utf-8' });
     } catch (e) {
         e.message = 'File not found: /proc/mdstat.\n' + e.message;
-        throw(e);
+        throw (e);
     }
 
     // parse file into json format
+    let result;
     try {
-        return parser.parse(file);
+        result = parser.parse(file);
     } catch (e) {
         e.message = 'Can not parse: /proc/mdstat.\n' + e.message;
-        throw(e);
+        throw (e);
     }
+
+    // resolve block name with unique linkage
+    const names = fs.readdirSync('/dev/md');
+    let raidNames = {};
+    for (const name of names) {
+        const blockName = path.basename(fs.readlinkSync('/dev/md/' + name));
+        raidNames[blockName] = name;
+    }
+
+    // insert uniqueName into result
+    for (const raid of result['raids']) {
+        raid['unique'] = raidNames[raid['name']];
+    }
+
+    return result;
 };
